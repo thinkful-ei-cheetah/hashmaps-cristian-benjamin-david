18b605b3e1fa57ca4ecc52bc287227d6c50c106c
David Nordeen <davidnordeen@gmail.com>
2019-06-17 09:41:48 -0700

    Finished 3

--- hashmap-drills.js
+++ hashmap-drills.js
@@ -1,31 +1,37 @@
-const HashMap = require('./hashmap')
+'use strict';
+
+const HashMap = require('./hashmap');
 
 function main() {
-  const lotr = new HashMap()
-  HashMap.MAX_LOAD_RATIO = 0.5
-  HashMap.SIZE_RATIO = 3
+  const lotr = new HashMap();
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
 
-  lotr.set('hobbit', 'bilbo')
-  lotr.set('hobbit', 'frodo')
-  lotr.set('wizard', 'gandolf')
-  lotr.set('human', 'aragon')
-  lotr.set('elf', 'legolas')
-  lotr.set('maiar', 'the necromancer')
-  lotr.set('maiar', 'sauron')
-  lotr.set('ringbearer', 'gollum')
-  lotr.set('ladyoflight', 'galadriel')
-  lotr.set('halfelven', 'arwen')
-  lotr.set('ent', 'treebeard')
+  lotr.set('hobbit', 'bilbo');
+  lotr.set('hobbit', 'frodo');
+  lotr.set('wizard', 'gandolf');
+  lotr.set('human', 'aragon');
+  lotr.set('elf', 'legolas');
+  lotr.set('maiar', 'the necromancer');
+  lotr.set('maiar', 'sauron');
+  lotr.set('ringbearer', 'gollum');
+  lotr.set('ladyoflight', 'galadriel');
+  lotr.set('halfelven', 'arwen');
+  lotr.set('ent', 'treebeard');
 
   // console.log(lotr);
 
-  console.log(lotr.get('maiar'))
-  console.log(lotr.get('hobbit'))
+  console.log(lotr.get('maiar'));
+  console.log(lotr.get('hobbit'));
 }
 
-main()
+main();
 
 // 1. yes
 // sauron, frodo
 // there is only one value returned for each of the keys.
 // capacity is 24 because our max load ratio was exceeded and a resize was triggered.
+
+
+//2. This function shows us that multiple .set()s using the same key will override the previously set() values.
+
